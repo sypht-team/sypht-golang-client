975559adc9c947e3634c7a94fb93172f10800188
Simon Mittag <simon@sypht.com>
2019-09-12 07:10:49 +1000

    shorter example

--- example/main.go
+++ example/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/sypht-team/sypht-golang-client"
@@ -12,36 +11,13 @@ const (
 )
 
 func main() {
-	client, err := sypht.NewSyphtClient(os.Getenv("SYPHT_API_KEY"), nil)
-	if err != nil {
-		log.Fatalf("Fail to initialize Sypht client %v", err)
-	}
-	token, err := client.RefreshToken()
-	if err != nil {
-		log.Println(err)
-	} else {
-		log.Printf("api token is %s", token)
-	}
+	client, _ := sypht.NewSyphtClient(os.Getenv("SYPHT_API_KEY"), nil)
 
-	resp, err := client.Upload(fileName, []string{
+	uploaded, _ := client.Upload(fileName, []string{
 		sypht.Invoice,
 		sypht.Document,
 	})
-	if err != nil {
-		log.Println(err)
-	} else {
-		sypht.PrettyPrintResponse(resp)
-	}
 
-	result, err := client.Results(resp["fileId"].(string))
-	if err != nil {
-		log.Println(err)
-	} else {
-		sypht.PrettyPrintResponse(result)
-	}
-
-	_, err = client.Image(resp["fileId"].(string), 1)
-	if err != nil {
-		log.Println(err)
-	}
+	result, _ := client.Results(uploaded["fileId"].(string))
+	sypht.PrettyPrintResponse(result)
 }
