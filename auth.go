2fd9dce03418cb906e8143ef328bea54926025f8
NOTYWTR <liuao1207@gmail.com>
2020-06-30 22:17:23 +1000

    new auth endpoint

--- auth.go
+++ auth.go
@@ -1,6 +1,7 @@
 package sypht
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -13,19 +14,33 @@ var gracePeriod = 10
 
 // RefreshToken refreshes api token and updates Sypht client instance
 func (s *Client) RefreshToken() (token string, err error) {
-	payload := strings.NewReader(strings.Join([]string{"{\n \"client_id\":\"",
-		s.config.clientID,
-		"\",\n \"client_secret\":\"",
-		s.config.clientSecret,
-		"\",\n \"audience\":\"https://api.sypht.com\",\n \"grant_type\":\"client_credentials\" \n}"}, ""))
-
-	req, err := http.NewRequest("POST", s.config.authURL, payload)
-	if err != nil {
-		log.Println(err)
-		return
+	var req *http.Request
+	var payload *strings.Reader
+	if strings.Contains(s.config.authURL, "/oauth2") {
+		payload = strings.NewReader("client_id=" + s.config.clientID + "&grant_type=client_credentials")
+		basicAuthSlug := base64.StdEncoding.EncodeToString([]byte(strings.Join([]string{s.config.clientID, s.config.clientSecret}, ":")))
+		req, err = http.NewRequest("POST", s.config.authURL, payload)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Add("Authorization", "Basic "+basicAuthSlug)
+	} else {
+		payload = strings.NewReader(strings.Join([]string{"{\n \"client_id\":\"",
+			s.config.clientID,
+			"\",\n \"client_secret\":\"",
+			s.config.clientSecret,
+			"\",\n \"audience\":\"https://api.sypht.com\",\n \"grant_type\":\"client_credentials\" \n}"}, ""))
+		req, err = http.NewRequest("POST", s.config.authURL, payload)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/json")
 	}
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
 
 	res, err := s.httpClient.Do(req)
 	if err != nil {
